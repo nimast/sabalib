40334d144fba22fdcd2a49cc943c6a4e421cfdbd
Nimrod Astrahan <nimast@gmail.com>
2014-05-17 19:26:25 +0300

    Bogus part - initial implementation

--- code/Sabalib/Sabalib.h
+++ code/Sabalib/Sabalib.h
@@ -2,16 +2,44 @@
 #define SABALIB_H
 
 #include "Arduino.h"
+#include <Adafruit_NeoPixel.h>
 
-class PartBase {
+class Section
+{
+  public:
+    Section(int startIndex, int endIndex, Adafruit_NeoPixel *strip);
+    ~Section();
+    void begin(void);
+    void show(void);
+    Adafruit_NeoPixel *strip;
+  protected:
+    int startIndex;
+    int endIndex;
+};
 
- public:
-  
-  PartBase();
-  ~PartBase();
+class PartBase
+{
+  public:
+    PartBase(Section *sections, int numberOfSections);
+    ~PartBase();
+    virtual void tick(void);
+  protected:
+    Section *sections;
+    int numberOfSections;
+};
 
-  void tick(void);
-
+class BogusPart: public PartBase
+{
+  protected:
+    int blink_direction;
+    int pulse_step;
+    int pulse_max;
+    int pulse_index;
+    int pulse_current_level;
+  public:
+    BogusPart(Section *sections, int numberOfSections);
+    ~BogusPart();
+    void tick(void);
 };
 
 #endif // SABALIB_H
